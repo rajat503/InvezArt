68099c64be7ec553379a17298fb2f3b2c83b7bd1
Rajat Agarwal <rajat503@gmail.com>
2015-03-30 18:01:33 +0530

    linked queries with routes

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from queries import companywise
+from queries import ratiowise
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,11 +9,11 @@ def hello_world():
 
 @app.route('/company/<company_symbol>')
 def show_company(company_symbol):
-	return 'Company %s' % company_symbol
+	return companywise(company_symbol)
 
 @app.route('/ratio/<ratio_name>/<float:lower_limit>/<float:upper_limit>')
 def show_ratio(ratio_name, lower_limit, upper_limit):
-	return 'Ratio %s' % ratio_name
+	return ratiowise(ratio_name, lower_limit, upper_limit)
 
 if __name__ == '__main__':
 	app.debug = True
--- queries.py
+++ queries.py
@@ -1,5 +1,33 @@
+# from database import db_session
+
+# @app.teardown_appcontext
+# def shutdown_session(exception=None):
+#     db_session.remove()
+
+from models import Company
 from database import db_session
 
-@app.teardown_appcontext
-def shutdown_session(exception=None):
-    db_session.remove()
+def ratiowise(ratio_name, lower_limit, upper_limit):
+	ratios=["price", "dps", "eps", "dpr"]
+	try:
+		if ratio_name not in ratios:
+			return "Invalid Ratio"
+		else:
+			if(lower_limit==0.0 and upper_limit==0.0):
+				#query top 5 ratios
+			else:
+				if(upper_limit==0.0):
+					#query by lowerlimit
+				else:
+					#query between upper and lower
+	except:
+		return "Error"
+
+def companywise(company_symbol):
+	try:
+		company_query=Company.query.filter_by(symbol=company_symbol).first()
+		return company_query.name
+	except:
+		return "Error"
+
+
